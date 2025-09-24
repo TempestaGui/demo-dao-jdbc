9adaa620b2c4ad5c72866e8e0f4b0c463c71c9b6
tempestaGui <guilhermeftempesta@gmail.com>
2025-09-23 21:33:22 -0300

    seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department dp = new Department( "books",1);
+
+        Seller s1 = new Seller(1,"John","John@gmail",new Date(),3000.0,dp);
+        System.out.println(s1);
         System.out.println(dp.toString());
     }
 }
